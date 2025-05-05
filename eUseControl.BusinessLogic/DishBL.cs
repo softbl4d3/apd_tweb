0c278dd8e70482f3dd368645d97be0db02c01c78
softbl4d3 <dezik290@gmail.com>
2025-05-05 17:11:25 +0300

    dish Bl arch fix

--- eUseControl.BusinessLogic/DishBL.cs
+++ eUseControl.BusinessLogic/DishBL.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.DTO;
+using eUseControl.Domain.Entities.Resps;
 
 namespace eUseControl.BusinessLogic
 {
-    internal class DishBL
+    public class DishBL : DishApi, IDish
     {
+        public AdminResp AddDish(DishDTO dish) => AddDishAction(dish);
+        public AdminResp EditDish(DishDTO dish) => EditDishAction(dish);
+        public AdminResp DeleteDish(int Id) => DeleteDishAction(Id);
+        public DishDTO GetDishById(int Id) => GetDishByIdAction(Id);
+        public List<DishDTO> GetAllDishes() => GetAllDishesAction();
+
     }
 }
